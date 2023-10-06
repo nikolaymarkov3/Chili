fe0d8f4b35a4dd62b3a36adfb9c572b213acab0c
Nikolai Markow <i@nikomar.ru>
2023-10-06 20:41:47 +0700

    First commit

---
+++ src/test/java/runner/BaseTest.java
@@ -0,0 +1,56 @@
+package runner;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+
+public abstract class BaseTest {
+
+    private WebDriver driver;
+    private WebDriverWait webDriverWait20;
+    private WebDriverWait webDriverWait10;
+    private WebDriverWait webDriverWait5;
+
+    @BeforeMethod
+    protected void beforeMethod() {
+        driver = BaseUtils.createDriver();
+    }
+
+    @AfterMethod
+    protected void afterMethod() {
+        driver.quit();
+        webDriverWait20 = null;
+        webDriverWait10 = null;
+        webDriverWait5 = null;
+    }
+
+    protected WebDriver getDriver() {
+        return driver;
+    }
+
+    protected WebDriverWait getWait20() {
+        if (webDriverWait20 == null) {
+            webDriverWait20 = new WebDriverWait(driver, Duration.ofSeconds(20));
+        }
+
+        return webDriverWait20;
+    }
+
+    protected WebDriverWait getWait10() {
+        if (webDriverWait10 == null) {
+            webDriverWait10 = new WebDriverWait(driver, Duration.ofSeconds(10));
+        }
+
+        return webDriverWait10;
+    }
+
+    protected WebDriverWait getWait5() {
+        if (webDriverWait5 == null) {
+            webDriverWait5 = new WebDriverWait(driver, Duration.ofSeconds(5));
+        }
+
+        return webDriverWait5;
+    }
+}
